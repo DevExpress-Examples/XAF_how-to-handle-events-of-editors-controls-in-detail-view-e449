bd169a91a700e0a83ac1ea3ba622ac2e35aec0f4
DevExpressExampleBot <fakeEmail>
2023-03-01 15:05:52 +0400

    Source auto update [skip ci]

--- VB/WinSample.Win/Program.vb
+++ VB/WinSample.Win/Program.vb
@@ -1,36 +1,30 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Configuration
 Imports System.Windows.Forms
-
 Imports DevExpress.ExpressApp
 Imports DevExpress.ExpressApp.Security
-Imports DevExpress.ExpressApp.Win
-Imports DevExpress.Persistent.Base
-Imports DevExpress.Persistent.BaseImpl
 
 Namespace WinSample.Win
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
-			Dim _application As New WinSampleWindowsFormsApplication()
-			_application.ConnectionString = Demo.CodeCentralExampleInMemoryDataStoreProvider.ConnectionString
-			Try
-    DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.Register()
-    				_application.ConnectionString = DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.ConnectionString
-				_application.Setup()
-				_application.Start()
-			Catch e As Exception
-				_application.HandleException(e)
-			End Try
-		End Sub
-	End Class
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+            Dim _application As WinSampleWindowsFormsApplication = New WinSampleWindowsFormsApplication()
+            _application.ConnectionString = Demo.CodeCentralExampleInMemoryDataStoreProvider.ConnectionString
+            Try
+                Xpo.InMemoryDataStoreProvider.Register()
+                _application.ConnectionString = Xpo.InMemoryDataStoreProvider.ConnectionString
+                _application.Setup()
+                _application.Start()
+            Catch e As Exception
+                _application.HandleException(e)
+            End Try
+        End Sub
+    End Module
 End Namespace
--- VB/WinSample.Win/WinApplication.vb
+++ VB/WinSample.Win/WinApplication.vb
@@ -1,21 +1,23 @@
 Imports DevExpress.ExpressApp
 Imports DevExpress.ExpressApp.Xpo
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.ExpressApp.Win
 
 Namespace WinSample.Win
-	Partial Public Class WinSampleWindowsFormsApplication
-		Inherits WinApplication
+
+    Public Partial Class WinSampleWindowsFormsApplication
+        Inherits WinApplication
+
         Protected Overrides Sub CreateDefaultObjectSpaceProvider(ByVal args As CreateCustomObjectSpaceProviderEventArgs)
             args.ObjectSpaceProvider = New XPObjectSpaceProvider(args.ConnectionString, args.Connection)
         End Sub
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub WinSampleWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
-			e.Updater.Update()
-			e.Handled = True
-		End Sub
-	End Class
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub WinSampleWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DatabaseVersionMismatchEventArgs)
+            e.Updater.Update()
+            e.Handled = True
+        End Sub
+    End Class
 End Namespace
